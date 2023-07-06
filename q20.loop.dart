679d0697390317eb703d60627df4d707774241f9
codewithnimraa <136242525+codewithnimraa@users.noreply.github.com>
2023-07-06 15:54:24 +0500

    Add files via upload

---
+++ q20.loop.dart
@@ -0,0 +1,19 @@
+void main() {
+  String input = "nimra";
+  int vowelCount = 0;
+
+  for (int i = 0; i < input.length; i++) {
+    String character = input[i].toLowerCase();
+
+    if (character == 'a' ||
+        character == 'e' ||
+        character == 'i' ||
+        character == 'o' ||
+        character == 'u') {
+      vowelCount++;
+    }
+  }
+
+  print("Number of vowels: $vowelCount");
+}
+
