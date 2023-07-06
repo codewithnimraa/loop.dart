683ff232139271eba149efb21227e00f109a2b95
codewithnimraa <136242525+codewithnimraa@users.noreply.github.com>
2023-07-06 15:55:02 +0500

    Add files via upload

---
+++ q22.loop.dart
@@ -0,0 +1,13 @@
+  void main() {
+  List<int> numbers = [1, 2, 3];
+  int sumOfSquares = 0;
+
+  for (int number in numbers) {
+    if (number % 2 != 0) {
+      sumOfSquares += (number * number);
+    }
+  }
+
+  print("Sum of squares of odd numbers: $sumOfSquares");
+}
+
