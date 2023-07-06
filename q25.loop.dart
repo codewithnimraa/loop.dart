1c5934885b439a6c1e696831eb68b957dbf8b3c3
codewithnimraa <136242525+codewithnimraa@users.noreply.github.com>
2023-07-06 15:56:00 +0500

    Add files via upload

---
+++ q25.loop.dart
@@ -0,0 +1,26 @@
+void main() {
+  List<int> numbers = [4, 7, 10, 13, 31];
+  List<int> primeNumbers = [];
+
+  for (int number in numbers) {
+    if (isPrime(number)) {
+      primeNumbers.add(number);
+    }
+  }
+
+  print("Prime numbers: $primeNumbers");
+}
+
+bool isPrime(int number) {
+  if (number <= 1) {
+    return false;
+  }
+
+  for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
